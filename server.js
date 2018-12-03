b35ea146fd1ace49f223618a5d59381e6c53aef2
VictorCam <campav@oregonstate.edu>
2018-12-03 10:06:54 -0800

    v2

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 var path = require('path');
 var express = require('express');
 var app = express();
+//var data = require('./data');
+
+
 
 app.use(express.static('public'));
 
 app.set('port', (process.env.PORT || 3000));
 
 app.get('/', function (req, res) {
-  res.status(200).sendFile(path.join(__dirname, 'public', 'index.html'))
+  res.status(200).render('index');
 });
+
+app.get('*', function (req, res) {
+   res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
 
 app.listen(app.get('port'), function(){
   console.log('== server started on port 3000');
