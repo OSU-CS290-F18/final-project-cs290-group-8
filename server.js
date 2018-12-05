d7eb67d76b046ad0e07dceede031d1ea0e90b53b
matsumon <matsumon@oregonstate.edu>
2018-12-04 18:03:53 -0800

    mongo added

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ var exphbs = require('express-handlebars');
 var express = require('express');
 var app = express();
 var data = require('./data');
+var mongoClient = require("mongodb").MongoClient;
+var mongoHost = "classmongo.engr.oregonstate.edu";
+var mongoPort = 27017;
+var mongoUsername = "cs290_matsumon";
+var mongoPassword = "cs290_matsumon";
+var mongoDBName = "cs290_matsumon";
+var mongoURL = "mongodb://" + mongoUsername + ":" + mongoPassword + "@" + 
+mongoHost + ":" + mongoPort + "/" + mongoDBName;
+var mongoDB = null;
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -10,9 +19,9 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
-app.set('port', (process.env.PORT || 3444);
+app.set('port', (process.env.PORT || 3434));
 
-app.get('/', function (req, res) {
+app.get('/:personId', function (req, res) {
   res.status(200).render('index', {post: data});
 });
 
@@ -21,5 +30,16 @@ app.get('*', function (req, res) {
 });
 
 app.listen(app.get('port'), function(){
-  console.log('== server started on port 3000');
+	mongoClient.connect(mongoURL,function(err,client)
+		{
+			if(err)
+	{
+		throw err;
+	}
+			else
+	{
+		mongoDB = client.db(mongoDBName);
+		console.log("mongo and server connected. port 3434");
+	}
+		});	
 });
